01912ea87654a98f2d10d06818ee2e5b290d2eee
mathieuforest <mathieu.forest@exob2b.com>
2017-11-16 17:53:09 -0500

    db

--- config/dbconf.js
+++ config/dbconf.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql2');
 var dbconfig = {
-        'host' : 'localhost',
-        'user' : 'root',
-        'password' : '',
+        'host' : 'https://www.cleardb.com/database/details?id=8E5D9977FF74AAB54D9FF79360AF4730',
+        'user' : 'b8624f64a5a8f1',
+        'password' : '6aaaa6be',
         'database' : 'foodTrucks',
         'supportBigNumbers' : true
 };
