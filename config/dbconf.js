8b9087af54dc5a757828b9cb72454b65e8067f53
mathieuforest <mathieu.forest@exob2b.com>
2017-11-16 18:12:52 -0500

    db

--- config/dbconf.js
+++ config/dbconf.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql2');
 var dbconfig = {
-        'host' : 'us-cdbr-iron-east-02.cleardb.net',
+        'host' : 'us-cdbr-iron-east-05.cleardb.net',
         'user' : 'b8624f64a5a8f1',
         'password' : '6aaaa6be',
         'database' : 'heroku_073aa83fcab1a66',
