10cf7bf5535adde5f4dd0bc56cc36629fe7bfa8b
mathieuforest <mathieu.forest@exob2b.com>
2017-11-16 18:31:39 -0500

    port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var controlleurAccount = require('./api/createAccount')
 
 var app = express();
 var server = http.createServer(app);
-server.listen(3000);
+server.listen(port);
 
 app.use(express.static(__dirname));//to get also css, js,...
 app.use(bodyParser.json()); // support json encoded bodies
